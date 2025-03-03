77f30d083daaab5e6c478550b000400f18e514d5
Amr Waheed <amrmohamed877@gmail.com>
2025-03-03 06:49:14 +0200

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { HiArrowCircleDown } from "react-icons/hi";
 import "./App.css";
 import { FiSettings } from "react-icons/fi";
 import { BrowserRouter } from "react-router-dom";
@@ -7,9 +6,17 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <div className="flex relative dark:bg-main-dark-bg">
+        <div className="dark:bg-main-dark-bg relative flex">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
-            <TooltipComponent></TooltipComponent>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                className="hover:bg-light-gray p-3 text-3xl text-white hover:drop-shadow-xl"
+                type="button"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
           </div>
         </div>
       </BrowserRouter>
---
+++ src/components/Hi.jsx
@@ -0,0 +1,8 @@
+function Hi() {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default Hi
